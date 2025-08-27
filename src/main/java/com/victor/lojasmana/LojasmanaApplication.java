fa7cbe121d247633de9ccf55e813302b4917bb01
Victor Almada <victor_mathers@hotmail.com>
2025-08-27 18:07:32 -0300

    Primeiro Commit

---
+++ src/main/java/com/victor/lojasmana/LojasmanaApplication.java
@@ -0,0 +1,13 @@
+package com.victor.lojasmana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojasmanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojasmanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/lojasmana/LojasmanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.lojasmana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojasmanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
